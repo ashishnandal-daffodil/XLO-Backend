c8d845045fd0f7e91fa5357cd4a2de103d1ea139
Ashish <ashish.nandal@daffodilsw.com>
2022-09-13 18:21:48 +0530

    favorite update and insert

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Favorite as Favorite_Def, FavoriteDocument } from "src/schemas/favorite.schema";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  Favorite as Favorite_Def,
+  FavoriteDocument,
+} from 'src/schemas/favorite.schema';
 
 @Injectable()
 export class FavoritesService {
-  constructor(@InjectModel(Favorite_Def.name) private productModel: Model<FavoriteDocument>) {}
+  constructor(
+    @InjectModel(Favorite_Def.name)
+    private favoriteModel: Model<FavoriteDocument>,
+  ) {}
 
   async create(favoriteData) {
-    return this.productModel.insertMany(favoriteData);
+    let { user, product } = favoriteData || {};
+
+    let isFavorite = await this.favoriteModel.findOne({
+      user: user,
+      product: product,
+    });
+
+    if (!isFavorite) {
+      return this.favoriteModel.insertMany(favoriteData);
+    } else {
+      return this.favoriteModel.updateOne(
+        { _id: isFavorite._id },
+        favoriteData,
+      );
+    }
   }
 }
