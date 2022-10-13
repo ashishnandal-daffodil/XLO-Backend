8219fc8d2fd5e83692c7510970ac00be399c5401
ajuk07 <ajay.bhandari@unthinkable.com>
2022-10-13 15:35:23 +0530

    add product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,17 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors,UploadedFile } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 import { SharpService } from "nestjs-sharp";
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller("products")
 export class ProductsController {
   constructor(private readonly productsService: ProductsService,private sharpService: SharpService) {}
 
-  @Post()
+  @Post('addProduct')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
+
+  @Post('file')
+  @UseInterceptors(
+    FileInterceptor('file',{
+      storage:diskStorage({
+        destination: './uploads',
+        filename:(req,file,callback)=>{
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = extname(file.originalname);
+        const filename =`${uniqueSuffix}${ext}`;
+        callback(null,filename);
+
+        },
+      }),
+    }),
+  )
+  handleUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return 'File upload API';
+  }
+
 
   @Get('allProduct')
   async findAll(@Query() { skip, limit ,category}) {
